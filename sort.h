28fece69103f1907a4bd2408457863fee02e624d
Hana00661 <eng.hana95@gmail.com>
2024-01-22 21:01:50 +0400

    try

--- sort.h
+++ sort.h
@@ -28,6 +28,6 @@ void selection_sort(int *array, size_t size);
 int _part(int array[], int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 void _sort(int array[], int low, int high, size_t size);
-
+void shell_sort(int *array, size_t size);
 
 #endif
