106fe606b02aae9641c6bfad2987f9d1c0e9e97b
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-01-22 15:12:49 +0300

    commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,10 +17,23 @@ void insertion_sort_list(listint_t **list)
 	temp = (*list)->next;
 	while (temp)
 	{
-		flkjsdljfslkdjflksaj;skdjf algorithm
-		lkjaj fadjfa
-		jsaldjflksadjf
-		lsdjflkjds
-		
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+			{
+				temp->prev->next = temp;
+			}
+			print_list(*list);
+		}
+		temp = temp->next;
 	}
 }
